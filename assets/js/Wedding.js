b234f5b41f75d11b57b21a3231b73c6b95277f48
Mevada01 <harsh.b.mevada@gmail.com>
2022-10-17 16:36:33 +0530

    updated

--- assets/js/Wedding.js
+++ assets/js/Wedding.js
@@ -1,33 +1,33 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 };
 
-window.onscroll = () =>{
+window.onscroll = () => {
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 };
 
 var swiper = new Swiper(".home-slider", {
-  loop:true,
-  spaceBetween:20,
-  grabCursor:true,
+  loop: true,
+  spaceBetween: 20,
+  grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable:true,
+    clickable: true,
   },
 });
 
 var swiper = new Swiper(".service-slider", {
-  loop:true,
-  spaceBetween:20,
-  grabCursor:true,
+  loop: true,
+  spaceBetween: 20,
+  grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable:true,
+    clickable: true,
   },
   breakpoints: {
     450: {
@@ -43,12 +43,12 @@ var swiper = new Swiper(".service-slider", {
 });
 
 var swiper = new Swiper(".reviews-slider", {
-  loop:true,
-  spaceBetween:20,
-  grabCursor:true,
+  loop: true,
+  spaceBetween: 20,
+  grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable:true,
+    clickable: true,
   },
   breakpoints: {
     450: {
